59afe15c3be8804242128ab5060edd987f7b72d6
wangjiayun <wjy631438519@163.com>
2020-10-27 18:19:06 +0800

    commit

---
+++ components/CaseTemp/CaseTemp.js
@@ -0,0 +1,25 @@
+let app = getApp()
+var util = require('../../utils/util.js')
+
+Component({
+  properties: {
+
+  },
+  options: {
+    // addGlobalClass: true,
+  },
+  data: {
+    headHeight: 0,
+    avatar: ''
+  },
+
+  attached() {
+
+  },
+
+  pageLifetimes: {
+  },
+
+  methods: {
+  }
+})
